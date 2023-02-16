b66f887da84962521073cdac20a76013f87c227b
Andrew <103659333+Adreyko@users.noreply.github.com>
2023-02-16 14:45:04 +0200

    changes in the index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project';
+
+  inputValue = ''
+
+  passwordNow = ''
+
+
+  onInput(event?: any) {
+    this.inputValue = event.target.value
+    if (this.inputValue.length == 0) {
+      this.passwordNow = ''
+    }
+    console.log(this.passwordNow)
+    if (this.inputValue.length > 0 && this.inputValue.length < 8) {
+      this.passwordNow = 'bad'
+    }
+    if (this.inputValue.match(/(?=.*[A-Za-z]).{8,16}$/) || this.inputValue.match(/[0-9].{7,16}$/)  || this.inputValue.match(/(?=.*[#$@!%&*?]).{7,16}$/)) {
+      this.passwordNow = 'easy'
+    }
+    if (this.inputValue.match(/(?=.*[A-Za-z])(?=.*?[0-9]).{8,16}$/) || this.inputValue.match(/(?=.*[#$@!%&*?])(?=.*?[0-9]).{8,16}$/) || this.inputValue.match(/(?=.*[#$@!%&*?])(?=.*[A-Za-z]).{8,16}$/)) {
+      this.passwordNow = 'okay'
+    }
+    if (this.inputValue.match(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[#$@!%&*?]).{8,16}$/)) {
+      this.passwordNow = 'nice'
+    }
+
+  }
+
 }
